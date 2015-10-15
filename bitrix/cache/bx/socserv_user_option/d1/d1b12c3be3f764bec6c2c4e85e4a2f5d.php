7de72fd609db77a271592b81f0100aaaad5c0528
u136055 <vanolia13@gmail.com>
2015-10-15 20:53:04 +0300

    main site template have been created

--- bitrix/cache/bx/socserv_user_option/d1/d1b12c3be3f764bec6c2c4e85e4a2f5d.php
+++ bitrix/cache/bx/socserv_user_option/d1/d1b12c3be3f764bec6c2c4e85e4a2f5d.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001444895682';
-$dateexpire = '001444905682';
+$datecreate = '001444931101';
+$dateexpire = '001444941101';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";s:1:"N";}';
 return true;
 ?>
--- bitrix/managed_cache/MYSQL/b_rating_voting/8e/8eaf02eff4c37441a3eb5f98764bd6f7.php
+++ bitrix/managed_cache/MYSQL/b_rating_voting/8e/8eaf02eff4c37441a3eb5f98764bd6f7.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001444843856';
-$dateexpire = '001444930256';
+$datecreate = '001444931112';
+$dateexpire = '001445017512';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:0:{}}';
 return true;
 ?>
